0405f357c12d74e87802c30e8b9c2b7ed053a9bf
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-07 22:18:56 -0600

    CREATE: includes/funciones.php with debug function for debugging variables and sanitize(s) function for html string sanitization.

---
+++ includes/funciones.php
@@ -0,0 +1,15 @@
+<?php
+
+// Debugear variables
+function debugear($variable) : string {
+    echo "<pre>";
+    var_dump($variable);
+    echo "</pre>";
+    exit;
+}
+
+// Escapa / Sanitizar el HTML
+function s($html) : string {
+    $s = htmlspecialchars($html);
+    return $s;
+}
