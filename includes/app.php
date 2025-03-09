55c82cf9c38796e45798fa9cecf600515999621b
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-08 18:30:19 -0600

    ADD: call setDB method from ActiveRecord to establish database connection

--- includes/app.php
+++ includes/app.php
@@ -3,3 +3,7 @@
 require 'funciones.php';
 require 'database.php';
 require __DIR__ . '/../vendor/autoload.php';
+
+// ─── Conexión A La DB ────────────────────────────────────────────────────────
+use Model\ActiveRecord;
+ActiveRecord::setDB($db);
