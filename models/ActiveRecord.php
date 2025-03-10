9c18b0516d46838a1977e1c5df2f4af81d39b3f7
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-10 13:14:21 -0600

    ADD: consultarSQL, crearObjeto, atributos, sanitizarAtributos, and sincronizar methods in models/ActiveRecord.php for querying, sanitizing, and synchronizing DB data

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -30,4 +30,68 @@ class ActiveRecord {
         static::$alertas = [];
         return static::$alertas;
     }
+
+    // ─── Consulta SQL Para Crear Un Objeto En Memoria ────────────────────
+    public static function consultarSQL($query) {
+        // Consultar la base de datos
+        $resultado = static::$db->query($query);
+
+        // Iterar los resultados
+        $array = [];
+        while($registro = $resultado->fetch_assoc()) {
+            $array[] = static::crearObjeto($registro);
+        }
+
+        // Liberar memoria
+        $resultado->free();
+
+        // Retorno de resultados
+        return $array;
+    }
+
+    // Creacion del objeto para cada fila del registro de la DB
+    public static function crearObjeto($registro) {
+        $objeto = new static;
+
+        foreach($registro as $key => $value) {
+            if(property_exists($objeto, $value)) {
+                $objeto->$key = $value;
+            }
+        }
+
+        return $objeto;
+    }
+
+    // Identificar y unir los atributos de la BD
+    public function atributos() {
+        $atributos = [];
+
+        foreach(static::$columnasDB as $columna) {
+            if($columna === 'id') continue;
+            $atributos[$columna] = $this->$columna;
+        }
+
+        return $atributos;
+    }
+
+    // Sanitizar los datos antes de guardarlos en la BD
+    public function sanitizarAtributos() {
+        $atributos = $this->atributos();
+        $sanitizado = [];
+
+        foreach($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->scape_string($value);
+        }
+
+        return $sanitizado;
+    }
+
+    // Sincroniza BD con Objetos en memoria
+    public function sincronizar($args = []) {
+        foreach($args as $key => $value) {
+            if(property_exists($this, $key) && !is_null($value)) {
+                $this->$key = $value;
+            }
+        }
+    }
 }
