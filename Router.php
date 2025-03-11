6e9c8ede6d46424529a58cfdd143cad0b0c00ab5
FernandoMercado-Dev <fernandomercado.b@hotmail.com>
2025-03-11 15:44:28 -0600

    ADD: render method in Router.php to render different views dynamically into a variable

--- Router.php
+++ Router.php
@@ -23,7 +23,7 @@ class Router {
         // Arreglo de rutas protegidas
 
         // Obtencion de la URL y el metodo
-        $currentURL = $_SERVER['PATH_INFO'] ?? null;
+        $currentURL = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         // Verificacion de ruta actual con las del proyecto
@@ -40,5 +40,22 @@ class Router {
         } else {
             echo "Página No Encontrada o Ruta no válida";
         }
+    }
+
+    public function render($view, $datos = []) {
+
+        // Leer lo que le pasamos  a la vista
+        foreach($datos as $key => $value) {
+            $$key = $value;
+        }
+
+        // Almacenamiento en memoria durante un momento...
+        ob_start();
+
+        // entonces incluimos la vista en el layout
+        include_once __DIR__ . "/views/$view.php";
+        // Limpia el Buffer
+        $contenido = ob_get_clean(); 
+        include_once __DIR__ . '/views/layout.php';
     }
 }
